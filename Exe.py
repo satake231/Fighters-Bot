3f82052e85a3631aab1209c39e0b098db12d4c57
satake231 <r1ryoichiros2@gmail.com>
2023-04-12 16:00:02 +0900

    First Commit

---
+++ Exe.py
@@ -0,0 +1,290 @@
+from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
+
+import datetime
+
+import discord
+from discord.ext import tasks
+
+team_num = '8'
+team_name = 'ファイターズ'
+TOKEN = ''
+
+def WinLose(team_num, soup, home_visiter):
+    if home_visiter == 0:
+        parse = soup.find('p', class_='bb-score__homeLogo bb-score__homeLogo--npbTeam{}'.format(team_num))\
+            .find_parent('div').find_parent('a')
+        score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+        score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+        if score_of_home > score_of_away:
+            return 0
+        elif score_of_home < score_of_away:
+            return 1
+        elif score_of_home == score_of_away:
+            return 2
+        else:
+            return 3
+
+    elif home_visiter == 1:
+        parse = soup.find('p', class_='bb-score__awayLogo bb-score__awayLogo--npbTeam{}'.format(team_num))\
+            .find_parent('div').find_parent('a')
+        score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+        score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+        if score_of_home < score_of_away:
+            return 0
+        elif score_of_home > score_of_away:
+            return 1
+        elif score_of_home == score_of_away:
+            return 2
+        else:
+            return 3
+
+    else:
+        return 3
+
+def GetOpponentName(home_visiter, team_num, soup):
+    if home_visiter == 0:
+        parse = soup.find('p', class_='bb-score__homeLogo bb-score__homeLogo--npbTeam{}'
+                          .format(team_num)).find_next_siblings("p")
+        return parse[0].text
+
+    elif home_visiter == 1:
+        parse = soup.find('p', class_='bb-score__awayLogo bb-score__awayLogo--npbTeam{}'
+                          .format(team_num)).find_previous_siblings("p")
+        return parse[0].text
+
+
+client = discord.Client(intents=discord.Intents.all())
+
+
+# 1日に一回ループ
+@tasks.loop()
+async def loop():
+    now = datetime.datetime.now()
+    year = now.strftime('%Y')
+    month = now.strftime('%m')
+    day = now.strftime('%d')
+    oclock = now.strftime('%H')
+    minmin = now.strftime('%M')
+
+    print('Loop resumed at {}:{}:{}:{}:{}'.format(year, month, day, oclock, minmin))
+
+    yahoo_url = 'https://baseball.yahoo.co.jp/npb/schedule/?date='
+
+    access_url = yahoo_url + year + '-' + month + '-' + day
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(access_url) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            home = soup.find_all('p', class_='bb-score__homeLogo bb-score__homeLogo--npbTeam{}'.format(team_num))
+            away = soup.find_all('p', class_='bb-score__awayLogo bb-score__awayLogo--npbTeam{}'.format(team_num))
+
+    if len(home) == 1:
+        try:
+            parse = soup.find('p', class_='bb-score__homeLogo bb-score__homeLogo--npbTeam{}'
+                              .format(team_num)).find_parent('div').find_parent('a')
+            start_time = parse.find('time', class_='bb-score__status').text
+            start_hour = start_time[:2]
+            start_minutes = start_time[:2]
+            game_start = now.replace(hour=int(start_hour), minute=int(start_minutes), second=0, microsecond=0)
+            process_start = game_start + datetime.timedelta(hours=2)
+            sleep_time = (process_start - now).total_seconds()
+            print('Start Short Sleep at {}:{}:{}:{}:{} for {}seconds'.format(year, month, day, oclock, minmin, sleep_time))
+            await asyncio.sleep(sleep_time)
+        except:
+            pass
+
+        parse = soup.find('p', class_='bb-score__homeLogo bb-score__homeLogo--npbTeam{}'
+                          .format(team_num)).find_parent('div').find_parent('a')
+        status = parse.find('p', class_='bb-score__link').text
+
+        loop_flag = 0
+
+        while(loop_flag == 0):
+            if status == '試合終了' or '回' in status:
+                win_lose = WinLose(team_num=team_num, soup=soup, home_visiter=0)
+
+                if win_lose == 0:
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_home, score_of_away)
+
+                    opponent_name = GetOpponentName(home_visiter=0, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で勝利しました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+                    loop_flag = 1
+
+                elif win_lose == 1: #負けた時
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_home, score_of_away)
+
+                    opponent_name = GetOpponentName(home_visiter=0, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で敗北しました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+                    loop_flag = 1
+
+                elif win_lose == 2: #引き分けた時
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_home, score_of_away)
+
+                    opponent_name = GetOpponentName(home_visiter=0, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で引き分けました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+                    loop_flag = 1
+                else:
+                    loop_flag = 1
+            elif status == '試合中':
+                await asyncio.sleep(300)
+            else:
+                for member in client.get_all_members():
+                    if not member.bot:
+                        try:
+                            await member.send('本日の試合は中止になりました。')
+                        except:
+                            pass
+                loop_flag = 1
+
+    elif len(away) == 1:
+        try:
+            parse = soup.find('p', class_='bb-score__awayLogo bb-score__awayLogo--npbTeam{}'
+                              .format(team_num)).find_parent('div').find_parent('a')
+            start_time = parse.find('time', class_='bb-score__status').text
+            start_hour = start_time[:2]
+            start_minutes = start_time[:2]
+            game_start = now.replace(hour=int(start_hour), minute=int(start_minutes), second=0, microsecond=0)
+            process_start = game_start + datetime.timedelta(hours=2)
+            sleep_time = (process_start - now).total_seconds()
+            print('Start Short Sleep at {}:{}:{}:{}:{} for {}seconds'.format(year, month, day, oclock, minmin, sleep_time))
+            await asyncio.sleep(sleep_time)
+        except:
+            pass
+
+        parse = soup.find('p', class_='bb-score__awayLogo bb-score__awayLogo--npbTeam{}'
+                          .format(team_num)).find_parent('div').find_parent('a')
+
+        status = parse.find('p', class_='bb-score__link').text
+        loop_flag = 0
+
+        while(loop_flag == 0):
+
+            if status == '試合終了' or '回' in status:
+
+                win_lose = WinLose(team_num=team_num, soup=soup, home_visiter=1)
+
+                if win_lose == 0: #勝った時
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_away, score_of_home)
+
+                    opponent_name = GetOpponentName(home_visiter=1, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で勝利しました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+
+                    loop_flag = 1
+
+                elif win_lose == 1: #負けた時
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_away, score_of_home)
+
+                    opponent_name = GetOpponentName(home_visiter=1, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で敗北しました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+                    loop_flag = 1
+
+                elif win_lose == 2: #引き分けた時
+                    score_of_home = int(parse.find('span', class_='bb-score__score bb-score__score--left').text)
+                    score_of_away = int(parse.find('span', class_='bb-score__score bb-score__score--right').text)
+
+                    score = '{}-{}'.format(score_of_home, score_of_away)
+
+                    opponent_name = GetOpponentName(home_visiter=1, team_num=team_num, soup=soup)
+
+                    for member in client.get_all_members():
+                        if not member.bot:
+                            try:
+                                await member.send('--------【速報】--------\n日本ハムが{}に{}で引き分けました！！'
+                                                  .format(opponent_name, score))
+                            except:
+                                pass
+                    loop_flag = 1
+                else:
+                    loop_flag = 1
+            elif status == '試合中':
+                await asyncio.sleep(300)
+            else:
+                for member in client.get_all_members():
+                    if not member.bot:
+                        try:
+                            await member.send('本日の試合は中止になりました。')
+                        except:
+                            pass
+                loop_flag = 1
+    else:
+        print('No game exists today')
+    # await asyncio.sleep(30) #実験用 実装時は消す
+
+    now = datetime.datetime.now()
+    target_time = datetime.datetime(now.year, now.month, now.day, 12, 0, 0) # 今日の12時
+    if now > target_time:
+        # 今日の12時を過ぎている場合、明日の12時まで待機
+        target_time += datetime.timedelta(days=1)
+    wait_seconds = (target_time - now).total_seconds()
+    year = now.strftime('%Y')
+    month = now.strftime('%m')
+    day = now.strftime('%d')
+    oclock = now.strftime('%H')
+    minmin = now.strftime('%M')
+    print('Start Long Sleep at {}:{}:{}:{}:{} for {}seconds'.format(year, month, day, oclock, minmin, wait_seconds))
+    await asyncio.sleep(wait_seconds)
+
+
+#ループ処理実行
+@client.event
+async def on_ready():
+    print("logged in as " + client.user.name)
+    loop.start()
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
